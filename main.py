44096b7e0bb5f80db21cd2a6b761fe9b94249789
bassam111573 <basaambakr2011@gmail.com>
2025-08-03 12:08:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
